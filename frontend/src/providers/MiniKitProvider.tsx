29327a16aefcf04aa430ad5bb1dd6a5ffac2b046
James <jf358950@gmail.com>
2025-07-13 15:22:32 +0100

    Fix MiniKitProvider import and add app ID

--- frontend/src/providers/MiniKitProvider.tsx
+++ frontend/src/providers/MiniKitProvider.tsx
@@ -2,5 +2,5 @@ import { type ReactNode } from 'react';
 import { MiniKitProvider as Provider } from '@worldcoin/minikit-js/minikit-provider';
 
 export default function MiniKitProvider({ children }: { children: ReactNode }) {
-  return <Provider>{children}</Provider>;
+  return <Provider props={{ appId: 'app_bc75ea0f4623eb64e1814126df474de3' }}>{children}</Provider>;
 }
